5a04e26494ae94025b8f99cf9abe6020a3cb2ad9
Lulu <s9341729@gmail.com>
2023-09-02 12:54:02 +0800

    update import_data.py

--- data/import_data.py
+++ data/import_data.py
@@ -13,7 +13,7 @@ dbconfig = {
 cnxpool = pooling.MySQLConnectionPool(pool_name="mypool", pool_size=3, **dbconfig)
 
 current_directory = os.getcwd()
-json_file_path = os.path.join(current_directory, 'taipei-attractions.json')
+json_file_path = os.path.join(current_directory, 'data/taipei-attractions.json')
 
 with open(json_file_path, 'r') as f:
     data = json.load(f)
